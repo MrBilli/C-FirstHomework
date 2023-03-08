b439b5a518d4767d1f43c46682a4609f19c8e4bc
Alexey <leha.basanov123@gmail.com>
2023-03-08 13:46:45 +0300

    Fourth task

--- Program.cs
+++ Program.cs
@@ -88,3 +88,29 @@ else
     Console.WriteLine($"{num} - данное число является нечётным");
 }
 */
+
+/*
+Задача 8: Напишите программу, которая на вход принимает число (N),
+ а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+/*
+Console.WriteLine("Input a number > 1");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+while(i<=num)
+{
+    if(i%2==0)
+    {
+        Console.Write($"{i} ");
+    }
+    i+=1;
+}
+Console.Write($"- четные числа от 1 до {num}");
+*/

fb7a7591afe70f12a8fda26995d6db759cc02ab4
Alexey <leha.basanov123@gmail.com>
2023-03-08 13:12:51 +0300

    Second task

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
 
-Console.WriteLine("Input two numbers");
+/*
+Console.WriteLine("Input two different numbers");
 
 int num1 = Convert.ToInt32(Console.ReadLine());
 
@@ -17,11 +18,59 @@ if (num1>num2)
 {
     Console.WriteLine($"{num1} > {num2} - первое число больше");
 }
-else if(num1==num2)
-{
-    Console.WriteLine($"{num1} = {num2} - числа равны");
-}
 else
 {
     Console.WriteLine($"{num2} > {num1} - второе число больше");
 }
+*/
+
+/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+/*
+Console.WriteLine("Input three different numbers");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if(num1>num2)
+{
+    if(num1>num3)
+    {
+        Console.WriteLine($"{num1} - первое число является максимальным");
+    }
+    else
+    {
+        Console.WriteLine($"{num3} - третье число является максимальным");
+    }
+}
+else
+{
+    if(num2>num3)
+    {
+    
+        Console.WriteLine($"{num2} - второе число является максимальным");
+    }
+    else
+    {
+        Console.WriteLine($"{num3} - третье число является максимальным");
+    }
+}
+*/
+
+/*
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+ является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет
+*/
